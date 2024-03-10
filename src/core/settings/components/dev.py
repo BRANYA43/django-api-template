f9272a3c3d68b8e0e2f378d49278bf3c2ebf01de
BRANYA <tbd2504@gmail.com>
2024-03-10 19:06:15 +0200

    Add DB settings for dev and prod

--- src/core/settings/components/dev.py
+++ src/core/settings/components/dev.py
@@ -2,4 +2,26 @@
 Development settings
 """
 
+from core.settings import env
+from core.settings.components import BASE_DIR
+
 DEBUG = True
+
+if env.get('DOCKER_RUN', '').lower() in ('true', '1'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'HOST': env.get('POSTGRES_HOST'),
+            'PORT': env.get('POSTGRES_PORT', '5432'),
+            'NAME': env.get('POSTGRES_DB'),
+            'USER': env.get('POSTGRES_USER'),
+            'PASSWORD': env.get('POSTGRES_PASSWORD'),
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': str(BASE_DIR / '../db.sqlite3'),
+        }
+    }
