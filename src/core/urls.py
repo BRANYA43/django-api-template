4621e1e22896f677a30ed47390cbd0a30c71072f
BRANYA <tbd2504@gmail.com>
2024-03-10 18:26:45 +0200

    Add REST and docs to settings and urls

--- src/core/urls.py
+++ src/core/urls.py
@@ -18,10 +18,15 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+api_urls = [
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
+
+urlpatterns = [path('admin/', admin.site.urls), path('api/', include(api_urls))]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
